318e5ca6e3b2cd4027fca88e1e83e9210b490476
dijoncavalcante <dijoncavalcante@gmail.com>
2021-08-09 22:39:46 -0400

    chore: Add viewBinding, extensions, model

---
+++ app/src/main/java/com/dijon/todolist/datasource/TaskDataSource.kt
@@ -0,0 +1,13 @@
+package com.dijon.todolist.datasource
+
+import com.dijon.todolist.model.Task
+
+object TaskDataSource {
+    private val list = arrayListOf<Task>()
+
+    fun getList() = list
+
+    fun insertTask(task: Task) {
+        list.add(task.copy(id = list.size + 1))
+    }
+}
---
+++ app/src/main/java/com/dijon/todolist/model/Task.kt
@@ -0,0 +1,8 @@
+package com.dijon.todolist.model
+
+data class Task(
+    val title: String,
+    val hour: String,
+    val date: String,
+    val id: Int = 0
+)
