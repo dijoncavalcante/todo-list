cb8e8393440553604fabf518ad65d1e658d8958a
dijoncavalcante <dijoncavalcante@gmail.com>
2021-08-25 22:00:50 -0400

    chore: Add Logs for TaskRepository class

--- app/src/main/java/com/dijon/todolist/model/repository/TaskRepository.kt
+++ app/src/main/java/com/dijon/todolist/model/repository/TaskRepository.kt
@@ -1,5 +1,6 @@
 package com.dijon.todolist.model.repository
 
+import android.util.Log
 import com.dijon.todolist.model.dao.TaskDao
 import com.dijon.todolist.model.data.Task
 
@@ -8,18 +9,25 @@ class TaskRepository(private val taskDao: TaskDao) {
     suspend fun getAll(): List<Task> =
         taskDao.getAll()
 
-    suspend fun save(task: Task) {s
+    suspend fun save(task: Task) {
+        Log.d(TAG, "Task: title:${task.title} description:${task.description}")
         taskDao.save(task)
     }
 
     suspend fun update(task: Task) {
+        Log.d(TAG, "Task: id:${task.id} title:${task.title} description:${task.description}")
         taskDao.update(task)
     }
 
     suspend fun delete(id: Long) {
+        Log.d(TAG, "Task id: $id")
         taskDao.remove(id)
     }
 
     fun get(task: Task): Task =
         taskDao.get(task.id)
+
+    companion object {
+        const val TAG = "TaskRepository"
+    }
 }
